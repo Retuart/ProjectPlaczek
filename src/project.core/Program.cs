97155435c93b3536de17df7b15aa435bcd590a9e
Jakub Patelski <pzx104888@student.wsb.poznan.pl>
2024-01-19 21:23:36 +0000

    Add automigration

--- src/project.core/Program.cs
+++ src/project.core/Program.cs
@@ -42,6 +42,17 @@ public class Program
             pattern: "{controller=Home}/{action=Index}/{id?}");
         app.MapRazorPages();
 
+        using (var scope = app.Services.CreateScope())
+        {
+            var services = scope.ServiceProvider;
+
+            var context = services.GetRequiredService<ApplicationDbContext>();
+            if (context.Database.GetPendingMigrations().Any())
+            {
+                context.Database.Migrate();
+            }
+        }
+
         app.Run();
     }
 }
