f7732dc918cbaf335502b22e1391f066c8a9deb6
Jakub Patelski <pzx104888@student.wsb.poznan.pl>
2024-01-19 21:48:46 +0000

    first migration

--- src/project.core/Program.cs
+++ src/project.core/Program.cs
@@ -42,16 +42,16 @@ public class Program
             pattern: "{controller=Home}/{action=Index}/{id?}");
         app.MapRazorPages();
 
-        using (var scope = app.Services.CreateScope())
-        {
-            var services = scope.ServiceProvider;
+        // using (var scope = app.Services.CreateScope())
+        // {
+        //     var services = scope.ServiceProvider;
 
-            var context = services.GetRequiredService<ApplicationDbContext>();
-            if (context.Database.GetPendingMigrations().Any())
-            {
-                context.Database.Migrate();
-            }
-        }
+        //     var context = services.GetRequiredService<ApplicationDbContext>();
+        //     if (context.Database.GetPendingMigrations().Any())
+        //     {
+        //         context.Database.Migrate();
+        //     }
+        // }
 
         app.Run();
     }
