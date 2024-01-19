8b98d6b5df7aa0073d747d4a24118f482f938790
Jakub Patelski <pzx104888@student.wsb.poznan.pl>
2024-01-19 19:39:33 +0000

    fix(seancemodel) typo

--- src/project.core/Models/seanceModel.cs
+++ src/project.core/Models/seanceModel.cs
@@ -1,3 +1,5 @@
+using System;
+
 namespace project.core.Models;
 
 public class seanceModel
@@ -6,8 +8,9 @@ public class seanceModel
     public int id { get; set; }
     public int id_movie { get; set; }
     public virtual movieModel movie { get; set; }
-    public datetime start { get; set; }
-    public datetime end { get; set; }
+    public DateTime start { get; set; }
+    public DateTime end { get; set; }
     public int id_room { get; set; }
+    public virtual roomModel room { get; set; }
     
 }
