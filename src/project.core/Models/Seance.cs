183b0726b39f3000e8b3b2cee35698a5f317e979
Jakub Patelski <pzx104888@student.wsb.poznan.pl>
2024-01-21 13:14:07 +0000

    fix seance model

--- src/project.core/Models/Seance.cs
+++ src/project.core/Models/Seance.cs
@@ -7,11 +7,11 @@ public class Seance
 
     public int Id { get; set; }
     public int Id_movie { get; set; }
-    public virtual Movie Movie { get; set; }
+    public virtual Movie? Movie { get; set; }
     public int Id_room { get; set; }
-    public virtual Room Room { get; set; }
+    public virtual Room? Room { get; set; }
     public int[]? OrdersIds { get; set; }
-    public Order[]? Orders { get; set; }
+    public ICollection<Order>? Orders { get; set; }
 
     public DateTime Start { get; set; }
     public DateTime End() { return Start.AddMinutes(Movie.Duration); }
