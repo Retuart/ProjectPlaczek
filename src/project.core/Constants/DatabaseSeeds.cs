0ba3c3cad674cee5bf0ed44b0dfa74923e6f3e1d
Jakub Patelski <pzx104888@student.wsb.poznan.pl>
2024-01-22 18:37:13 +0000

    add DatabaseSeedsConstants

---
+++ src/project.core/Constants/DatabaseSeeds.cs
@@ -0,0 +1,15 @@
+namespace project.core.Constants;
+
+public static class DatabaseSeedsConstants
+{
+    public const string AdminRoleName = "Admin";
+    public const string UserRoleName = "User";
+
+    public const string SuperAdminUserName = "superadmin@localhost";
+    public const string SuperAdminEmail = "superadmin@localhost";
+    public const string SuperAdminPassword = "admin";
+
+    public const string DefaultUserUserName = "user@localhost";
+    public const string DefaultUserEmail = "user@localhost";
+    public const string DefaultUserPassword = "user";
+}
